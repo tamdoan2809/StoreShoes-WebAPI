01dd264fe7459b11bc93222c8d72b21574a2fa62
tamdoan2809 <147482110+tamdoan2809@users.noreply.github.com>
2025-02-21 10:47:46 +0700

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nhom_10_QuanLyShopGiayTheThaoSneaker.WebApiApplication" Language="C#" %>
---
+++ Models/GioHang.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nhom_10_QuanLyShopGiayTheThaoSneaker.Models
+{
+    public class GioHang
+    {
+        DbQLSGDataContext db = new DbQLSGDataContext();
+        public int iMaSP { set; get; }
+        public string sTenSP { set; get; }
+        public string Anh { set; get; }
+        public int iSoLuong { set; get; }
+        public double dDonGia { set; get; }
+        public double dThanhTien
+        {
+            get { return iSoLuong * dDonGia; }
+        }
+        public GioHang(int maSP)
+        {
+            iMaSP = maSP;
+            SanPham sp = db.SanPhams.Single(s => s.MaSP == iMaSP);
+            sTenSP = sp.TenSP;
+            Anh = sp.Anh;
+            dDonGia = double.Parse(sp.GiaBan.ToString());
+            iSoLuong = 1;
+        }
+    }
+}
