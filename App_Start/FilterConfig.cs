b2362657bc30ef727e067b087e6f7989b1ea93d6
tamdoan2809 <147482110+tamdoan2809@users.noreply.github.com>
2025-02-21 10:42:08 +0700

    Add files via upload

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Nhom_10_QuanLyShopGiayTheThaoSneaker
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
